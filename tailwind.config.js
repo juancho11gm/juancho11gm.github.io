5bbb139662ff81f127864ba9ed18c334b0b2be55
“jugonzalez” <jugonzalez@hugeinc.com>
2022-03-31 21:54:13 -0500

    feat: new website

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   content: ['./src/**/*.{html,js,jsx}'],
   theme: {
-    extend: {},
+    fontSize: {
+      lg: '54px',
+      md: '26px',
+    },
+    backgroundPosition: {
+      mid: '60% 90%',
+    },
+    backgroundImage: {
+      'hero-pattern':
+        "linear-gradient(rgba(0, 0, 0, .8),rgba(0, 0, 0, .5)), url('assets/banner.jpg')",
+    },
   },
   plugins: [],
 };
