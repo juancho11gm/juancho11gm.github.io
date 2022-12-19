f8871e37d533d1314fc3f6339f8f2ac612353951
juancho11gm <juansebastian11gm@gmail.com>
2022-12-19 18:03:31 -0500

    fix: books date of publishing

--- pages/api/books.ts
+++ pages/api/books.ts
@@ -3,7 +3,7 @@ export default function handler(req, res) {
     {
       title: 'Atomic Habits',
       author: 'James Clear',
-      date: '2021',
+      date: '2018',
       description: 'Good habits can improve our lives. James provides ideas based on scientific research to transform our habits. They allow us to shape our behavior and how to apply the changes to our systems to improve our lives. ',
       tags: ['🪴 Personal Growth', '📆 Habits', '🧠 Psychology'],
       imageUrl: '/assets/books/atomic-habits.webp',
@@ -12,7 +12,7 @@ export default function handler(req, res) {
     {
       title: 'The Psychology of money',
       author: 'Morgan Housel',
-      date: '2021',
+      date: '2020',
       description: 'Doing well with money isn’t necessarily about what you know. It’s about how you behave. And behavior is hard to teach, even to really smart people. 19 short stories exploring the strange ways people think about money and teaches you how to make better sense of one of life’s most important topics.',
       tags: ['🪴 Personal Growth', '🐷 Finance', '🧠 Psychology'],
       imageUrl: '/assets/books/the-psychology-of-money.webp',
@@ -21,7 +21,7 @@ export default function handler(req, res) {
     {
       title: 'Never split the difference',
       author: 'Chris Voss',
-      date: '2021',
+      date: '2016',
       description: 'Inteligencia emocional para conseguir acuerdos donde no se puede permitir una respuesta negativa. Experiencias compartidas por un negociador del FBI que sirven para los negocios y la vida. Las reglas están basadas en la experiencia.',
       tags: ['🪴 Personal Growth', '🤝 Negotiation', '🧠 Psychology'],
       imageUrl: '/assets/books/never-split-the-difference.webp',
@@ -30,7 +30,7 @@ export default function handler(req, res) {
     {
       title: 'Lo que debes saber del dinero y nunca te enseñaron',
       author: 'Danilo Raymond',
-      date: '2021',
+      date: '2019',
       description: 'Tips to financial success through stories. Stories about borrowing, saving, investing and spending.',
       tags: ['🪴 Personal Growth', '🐷 Finance'],
       imageUrl: '/assets/books/lo-que-debes-saber-del-dinero.webp',
