51116e43c1101997308d0b7badd4949de9fd9897
juancho11gm <juansebastian11gm@gmail.com>
2022-12-19 22:37:37 -0500

    fix: gap between posts

--- components/MorePosts/MorePosts.tsx
+++ components/MorePosts/MorePosts.tsx
@@ -12,7 +12,7 @@ const MorePosts = ({ posts }: Props) => {
 			<h2 className='mb-8 text-4xl font-bold tracking-tighter leading-tight'>
 				More Posts
 			</h2>
-			<div className='grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-10'>
+			<div className='grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-10 md:gap-y-32 mb-10'>
 				{posts.map((post) => (
 					<PostPreview
 						key={post.slug}
