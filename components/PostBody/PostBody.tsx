26548f3e1c61992be1559589b8f2260b776e9939
juancho11gm <juansebastian11gm@gmail.com>
2023-01-13 17:45:08 -0500

    feat: add swr blog

--- components/PostBody/PostBody.tsx
+++ components/PostBody/PostBody.tsx
@@ -1,10 +1,38 @@
 import { vscDarkPlus } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import ReactMarkdown from 'react-markdown';
-import { ReactNode } from 'react';
+import React, { ReactNode } from 'react';
 import remarkGfm from 'remark-gfm';
 import styles from './PostBody.module.css';
 import classNames from 'classnames';
+
+type HeadingProps = {
+	children: ReactNode & ReactNode[];
+	className: string;
+};
+
+const Heading = ({ className, children }: HeadingProps) => {
+	console.log({ className, children });
+	return <a href={`#`}>{children}</a>;
+};
+
+const generateSlug = (str: string) => {
+	str = str?.replace(/^\s+|\s+$/g, '');
+	str = str?.toLowerCase();
+	const from = 'àáãäâèéëêìíïîòóöôùúüûñç·/_,:;';
+	const to = 'aaaaaeeeeiiiioooouuuunc------';
+
+	for (let i = 0, l = from.length; i < l; i++) {
+		str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+	}
+
+	str = str
+		?.replace(/[^a-z0-9 -]/g, '')
+		.replace(/\s+/g, '-')
+		.replace(/-+/g, '-');
+
+	return str;
+};
 
 type CodeBlockProps = {
 	children: ReactNode & ReactNode[];
@@ -40,6 +68,25 @@ const PostBody = ({ content }: Props) => {
 							className={props.className || ''}
 						/>
 					),
+					h2: (props: any) => {
+						const arr = props.children;
+						let heading = '';
+
+						for (let i = 0; i < arr.length; i++) {
+							if (arr[i]?.type !== undefined) {
+								for (let j = 0; j < arr[i].props.children.length; j++) {
+									heading += arr[i]?.props?.children[0];
+								}
+							} else heading += arr[i];
+						}
+
+						const slug = generateSlug(heading);
+						return (
+							<h3 id={slug}>
+								<a href={`#${slug}`} {...props}></a>
+							</h3>
+						);
+					},
 				}}
 				remarkPlugins={[remarkGfm]}
 			/>
