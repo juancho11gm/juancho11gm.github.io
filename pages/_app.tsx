1a2d09e4ad55f916b2d3e05a4bdf37a4d5e6e069
juancho11gm <juansebastian11gm@gmail.com>
2023-05-30 12:24:52 -0500

    feat: add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,32 @@
-import { ThemeProvider } from 'next-themes';
-import { AppProps } from 'next/app';
-import '../styles/index.css';
+import { ThemeProvider } from "next-themes";
+import { AppProps } from "next/app";
+import Script from "next/script";
+import "../styles/index.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-	return (
-		<ThemeProvider attribute='class' defaultTheme='light'>
-			<Component {...pageProps} />
-		</ThemeProvider>
-	);
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-F5DTT051H9"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+				window.dataLayer = window.dataLayer || [];
+				function gtag(){dataLayer.push(arguments);}
+				gtag('js', new Date());
+				gtag('config', 'G-F5DTT051H9', {
+					page_path: window.location.pathname,
+				});
+				`,
+        }}
+      />
+      <ThemeProvider attribute="class" defaultTheme="light">
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
